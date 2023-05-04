4d946f6c6960aa0d168fa93e0a0b1e2988ec7b7f
SantaForse <santaforsell@gmail.com>
2023-05-04 16:37:18 +0200

    Add project files.

---
+++ Controllers/SignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalSol.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
